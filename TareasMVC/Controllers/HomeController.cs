29eee58f13561e736c30ad0ea0e114c7d2ff41d0
Jose <jruizgall@uoc.edu>
2023-08-23 11:02:43 +0200

    subida archivos

---
+++ TareasMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+using TareasMVC.Models;
+
+namespace TareasMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> localizer;
+
+        public HomeController(ILogger<HomeController> logger, 
+            IStringLocalizer<HomeController>localizer)
+        {
+            _logger = logger;
+            this.localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Saludo = localizer["Buenos días"];
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TareasMVC/Migrations/20230810133438_AdminRol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF NOT EXISTS(Select Id from AspNetRoles where Id = 'e943e1da-8c4b-4540-aa8c-15cdfb2e6383')
+BEGIN
+INSERT AspNetRoles (Id, [Name], [NormalizedName])
+VALUES ('e943e1da-8c4b-4540-aa8c-15cdfb2e6383', 'admin', 'ADMIN')
+END
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id = 'e943e1da-8c4b-4540-aa8c-15cdfb2e6383'");
+        }
+    }
+}
